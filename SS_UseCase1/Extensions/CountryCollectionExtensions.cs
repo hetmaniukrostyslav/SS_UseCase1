3bef68041fd3b5974d932520c621bf5d4c6027c8
Rostyslav Hetmaniuk <rostyslav.hetmaniuk@advancedmd.com>
2023-06-23 13:27:05 +0300

    UC#1 Create filter function by name

---
+++ SS_UseCase1/Extensions/CountryCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SS_UseCase1.Models;
+
+namespace SS_UseCase1.Extensions
+{
+	public static class CountryCollectionExtensions
+	{
+        public static IEnumerable<Country> FilterByName(this IEnumerable<Country> source, string name)
+        {
+            return source.Where(x => x.Name.Common.Contains(name, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
+
